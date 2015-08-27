023729d10bb24f7e7a005aac279848dc4ed104cb
中村太郎 <tarotaro57@gmail.com>
2015-08-28 01:49:44 +0900

    とりあえず、必要な関数を用意した

--- PlugIns/KonashiPlugin/Source/KonashiBPLibrary/Classes/KonashiActor.h
+++ PlugIns/KonashiPlugin/Source/KonashiBPLibrary/Classes/KonashiActor.h
@@ -8,6 +8,60 @@
  * 
  */
 
+UENUM(BlueprintType)		//"BlueprintType" is essential to include
+enum class EKonashiDigitalIOPinEnum :uint8
+{
+    KE_KonashiDigitalIO0 = 0 UMETA(DisplayName="KonashiDigitalIO0"),
+    KE_KonashiDigitalIO1 = 1 UMETA(DisplayName="KonashiDigitalIO1"),
+    KE_KonashiDigitalIO2 = 2 UMETA(DisplayName="KonashiDigitalIO2"),
+    KE_KonashiDigitalIO3 = 3 UMETA(DisplayName="KonashiDigitalIO3"),
+    KE_KonashiDigitalIO4 = 4 UMETA(DisplayName="KonashiDigitalIO4"),
+    KE_KonashiDigitalIO5 = 5 UMETA(DisplayName="KonashiDigitalIO5"),
+    KE_KonashiDigitalIO6 = 6 UMETA(DisplayName="KonashiDigitalIO6"),
+    KE_KonashiDigitalIO7 = 7 UMETA(DisplayName="KonashiDigitalIO7"),
+    KE_KonashiS1 = 8 UMETA(DisplayName="KonashiS1"),
+    KE_KonashiLED2 = 9 UMETA(DisplayName="KonashiLED2"),
+    KE_KonashiLED3 = 10 UMETA(DisplayName="KonashiLED3"),
+    KE_KonashiLED4 = 11 UMETA(DisplayName="KonashiLED4"),
+    KE_KonashiLED5 = 12 UMETA(DisplayName="KonashiLED5"),
+    KE_KonashiI2C_SDA = 13 UMETA(DisplayName="KonashiI2C_SDA"),
+    KE_KonashiI2C_SCL = 14 UMETA(DisplayName="KonashiI2C_SCL")
+};
+
+UENUM(BlueprintType)		//"BlueprintType" is essential to include
+enum class EKonashiLevelEnum :uint8
+{
+    KE_KonashiLevelLow	 = 0 UMETA(DisplayName="KonashiLevelLow"),
+    KE_KonashiLevelHigh = 1 UMETA(DisplayName="KonashiLevelHigh"),
+    KE_KonashiLevelUnknown = 255 UMETA(DisplayName="KonashiLevelUnknown"),
+};
+
+UENUM(BlueprintType)
+enum class EKonashiPinModeEnum :uint8
+{
+    KE_KonashiPinModeInput	= 0 UMETA(DisplayName="KonashiPinModeInput"),
+    KE_KonashiPinModeOutput = 1 UMETA(DisplayName="KonashiPinModeOutput"),
+    KE_KonashiPinModeNoPulls = 2 UMETA(DisplayName="KonashiPinModeNoPulls"),
+    KE_KonashiPinModePullup = 3 UMETA(DisplayName="KonashiPinModePullup")
+};
+
+UENUM(BlueprintType)		//"BlueprintType" is essential to include
+enum class EKonashiAnalogIOPinEnum :uint8
+{
+    KE_KonashiAnalogIO0	 = 0 UMETA(DisplayName="KonashiAnalogIO0"),
+    KE_KonashiAnalogIO1 = 1 UMETA(DisplayName="KonashiAnalogIO1"),
+    KE_KonashiAnalogIO2 = 2 UMETA(DisplayName="KonashiAnalogIO2"),
+};
+
+UENUM(BlueprintType)		//"BlueprintType" is essential to include
+enum class EKonashiPWMModeEnum :uint8
+{
+    KE_KonashiPWMModeDisable	 = 0 UMETA(DisplayName="KonashiPWMModeDisable"),
+    KE_KonashiPWMModeEnable = 1 UMETA(DisplayName="KonashiPWMModeEnable"),
+    KE_KonashiPWMModeEnableLED = 2 UMETA(DisplayName="KonashiPWMModeEnableLED"),
+};
+
+
 UCLASS()
 class  KONASHIBPLIBRARY_API AKonashiActor : public AActor
 {
@@ -17,8 +71,56 @@ class  KONASHIBPLIBRARY_API AKonashiActor : public AActor
     UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
     void Find();
     
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void FindWithName(const FString &deviceName);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void AnalogReadRequest(EKonashiAnalogIOPinEnum pin);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    int32 AnalogRead(EKonashiAnalogIOPinEnum pin);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void AnalogWrite(EKonashiAnalogIOPinEnum pin,int32 milliVolt);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    EKonashiLevelEnum DigitalRead(EKonashiDigitalIOPinEnum pin);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void DigitalWrite(EKonashiDigitalIOPinEnum pin,EKonashiLevelEnum value);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void PinMode(EKonashiDigitalIOPinEnum pin,EKonashiPinModeEnum mode);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void PwmDuty(EKonashiDigitalIOPinEnum pin, int32 duty);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void PwmLedDrive(EKonashiDigitalIOPinEnum pin,int32 ratio);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void PwmMode(EKonashiDigitalIOPinEnum pin,EKonashiPWMModeEnum mode);
+    
+    UFUNCTION(BlueprintCallable, Category="KonashiBPLibrary")
+    void PwmPeriod(EKonashiDigitalIOPinEnum pin,int32 period);
     
     UFUNCTION(Category="KonashiBPLibrary", BlueprintImplementableEvent, meta=(DisplayName  = "Find Device Event CallBack Event"))
     void OnFindDeviceEvent(const TArray<FString> &deviceNames);
-	
+    
+     UFUNCTION(Category="KonashiBPLibrary", BlueprintImplementableEvent, meta=(DisplayName  = "Konashi Connected Event"))
+     void OnConnected();
+    
+    UFUNCTION(Category="KonashiBPLibrary", BlueprintImplementableEvent, meta=(DisplayName  = "Konashi Redy Event"))
+    void OnReady();
+    
+    UFUNCTION(Category="KonashiBPLibrary", BlueprintImplementableEvent, meta=(DisplayName  = "Konashi Redy Event"))
+    void OnDigitalInputDidChangeValue(EKonashiDigitalIOPinEnum pin,int32 value);
+    
+    UFUNCTION(Category="KonashiBPLibrary", BlueprintImplementableEvent, meta=(DisplayName  = "Konashi Redy Event"))
+    void OnAnalogPinDidChangeValue(EKonashiAnalogIOPinEnum pin,int32 value);
 };
+
+
+
+
+
--- PlugIns/KonashiPlugin/Source/KonashiBPLibrary/Private/KonashiActor.cpp
+++ PlugIns/KonashiPlugin/Source/KonashiBPLibrary/Private/KonashiActor.cpp
@@ -5,6 +5,65 @@
 #include  "Konashi.h"
 #endif
 
+int EKonashiDigitalIOPinEnumToKonashiDigitalIOPin(EKonashiDigitalIOPinEnum pin){
+    if(pin <= EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO7){
+        return (int)pin;
+    }else if(pin >= EKonashiDigitalIOPinEnum::KE_KonashiS1 && pin < EKonashiDigitalIOPinEnum::KE_KonashiI2C_SDA){
+        return (int)pin - (int)EKonashiDigitalIOPinEnum::KE_KonashiS1;
+    }else{
+        return (int)pin - (int)EKonashiDigitalIOPinEnum::KE_KonashiI2C_SDA + KonashiI2C_SDA;
+    }
+}
+
+EKonashiDigitalIOPinEnum KonashiDigitalIOPinToEKonashiDigitalIOPinEnum(KonashiDigitalIOPin pin){
+    switch (pin) {
+        case KonashiDigitalIO0:
+            return EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO0;
+        case KonashiDigitalIO1:
+            return EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO1;
+        case KonashiDigitalIO2:
+            return EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO2;
+        case KonashiDigitalIO3:
+            return EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO3;
+        case KonashiDigitalIO4:
+            return EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO4;
+        case KonashiDigitalIO5:
+            return EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO5;
+        case KonashiDigitalIO6:
+            return EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO6;
+        case KonashiDigitalIO7:
+            return EKonashiDigitalIOPinEnum::KE_KonashiDigitalIO7;
+        default:
+            break;
+    }
+}
+EKonashiAnalogIOPinEnum KonashiDigitalIOPinToEKonashiAnalogIOPinEnum(KonashiAnalogIOPin pin){
+    switch (pin) {
+        case KonashiAnalogIO0:
+            return EKonashiAnalogIOPinEnum::KE_KonashiAnalogIO0;
+        case KonashiAnalogIO1:
+            return EKonashiAnalogIOPinEnum::KE_KonashiAnalogIO1;
+        case KonashiAnalogIO2:
+            return EKonashiAnalogIOPinEnum::KE_KonashiAnalogIO2;
+    }
+}
+
+
+int EKonashiLevelEnumToKonashiLevel(EKonashiLevelEnum pin){
+    if(pin < EKonashiLevelEnum::KE_KonashiLevelUnknown){
+        return (int)pin;
+    }else{
+        return -1;
+    }
+}
+
+int EKonashiPinModeEnumToKonashiPinMode(EKonashiPinModeEnum pin){
+    if(pin >= EKonashiPinModeEnum::KE_KonashiPinModeNoPulls){
+        return (int)pin - (int)EKonashiPinModeEnum::KE_KonashiPinModeNoPulls;
+    }else{
+        return (int)pin;
+    }
+}
 
 
 void AKonashiActor::Find(){
@@ -23,22 +82,91 @@ void AKonashiActor::Find(){
 #endif
 }
 
+void AKonashiActor::FindWithName(const FString &deviceName){
+#if PLATFORM_IOS
+    if(deviceName == NULL){
+        Find();
+    }else{
+        [Konashi findWithName:[[NSString alloc ] initWithUTF8String:TCHAR_TO_UTF8(*deviceName)]];
+    }
+#endif
+}
 
 AKonashiActor::AKonashiActor(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 #if PLATFORM_IOS
     [[Konashi shared] setConnectedHandler:^{
-     NSLog(@"CONNECTED");
+        NSLog(@"Konashi CONNECTED");
+        OnConnected();
      }];
     [[Konashi shared] setReadyHandler:^{
-     NSLog(@"READY");
-     
+        NSLog(@"Konashi READY");
+        OnReady();
+     }];
+    
+    [[Konashi shared] setDigitalInputDidChangeValueHandler:^( KonashiDigitalIOPin pin , int value ){
+        NSLog(@"Konashi Input Change");
+        OnDigitalInputDidChangeValue(KonashiDigitalIOPinToEKonashiDigitalIOPinEnum(pin),value);
+     }];
+    
+    [[Konashi shared] setAnalogPinDidChangeValueHandler:^( KonashiAnalogIOPin pin , int value ) {
+        NSLog(@"Konashi Analog Input");
+        OnAnalogPinDidChangeValue(KonashiDigitalIOPinToEKonashiAnalogIOPinEnum(pin),value);
      }];
 #endif
 }
 
 
+void AKonashiActor::AnalogReadRequest(EKonashiAnalogIOPinEnum pin){
+#if PLATFORM_IOS
+#endif
+}
 
+int32 AKonashiActor::AnalogRead(EKonashiAnalogIOPinEnum pin){
+#if PLATFORM_IOS
+    return 1;
+#endif
+}
 
+void AKonashiActor::AnalogWrite(EKonashiAnalogIOPinEnum pin,int32 milliVolt){
+#if PLATFORM_IOS
+#endif
+}
+
+EKonashiLevelEnum AKonashiActor::DigitalRead(EKonashiDigitalIOPinEnum pin){
+#if PLATFORM_IOS
+    return EKonashiLevelEnum::KE_KonashiLevelLow;
+#endif
+}
+
+void AKonashiActor::DigitalWrite(EKonashiDigitalIOPinEnum pin,EKonashiLevelEnum value){
+#if PLATFORM_IOS
+#endif
+}
+
+void AKonashiActor::PinMode(EKonashiDigitalIOPinEnum pin,EKonashiPinModeEnum mode){
+#if PLATFORM_IOS
+#endif
+}
+
+void AKonashiActor::PwmDuty(EKonashiDigitalIOPinEnum pin,int32 duty){
+#if PLATFORM_IOS
+#endif
+}
+
+void AKonashiActor::PwmLedDrive(EKonashiDigitalIOPinEnum pin,int32 ratio){
+#if PLATFORM_IOS
+#endif
+}
+
+void AKonashiActor::PwmMode(EKonashiDigitalIOPinEnum pin,EKonashiPWMModeEnum mode){
+#if PLATFORM_IOS
+#endif
+}
+
+void AKonashiActor::PwmPeriod(EKonashiDigitalIOPinEnum pin,int32 period){
+#if PLATFORM_IOS
+#endif
+}
 
